b5148842a8bf5e62561eaefc17a897ee1ba2261b
Ben MacDonald <benjamin.t.macdonald1@gmail.com>
2023-06-22 15:42:33 -0400

    Added updates to multiple files

--- scratchpad.py
+++ scratchpad.py
@@ -4,3 +4,8 @@ import numpy as np
 
 fig, ax = plt.subplots()  # Create a figure containing a single axes.
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the axes.
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
